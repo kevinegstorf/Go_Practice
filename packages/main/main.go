933f0cedbf2f6073920ca61cbf14a9d50dae51c5
kevinegstorf <kevin.egstorf@gmail.com>
2017-03-11 12:48:59 +0100

    practice with packages and scope

---
+++ packages/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kevinegstorf/first_go/packages/icomefromamsterdam"
+	"github.com/kevinegstorf/first_go/packages/stringutil"
+	//someAlias "github.com/kevinegstorf/first_go/packages/icomefromamsterdam"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+	fmt.Println(winniepooh.BearName)
+}
